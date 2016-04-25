460e4a521c664d02aebbdadd01b410b0957c9796
zachariahtaylor <zachariah.taylor@wsu.edu>
2016-04-24 20:27:41 -0700

    Changed server port back to 80.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);
 
 
-app.listen(8000, function () {
+app.listen(80, function () {
     console.log('Server listening on port 80.');
 });
 
