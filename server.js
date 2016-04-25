86fbb2b8d7d8b0afaec7484c60ce1a9eb642d962
zachariahtaylor <zachariah.taylor@wsu.edu>
2016-04-24 19:54:17 -0700

    Changed port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);
 
 
-app.listen(80, function () {
+app.listen(8000, function () {
     console.log('Server listening on port 80.');
 });
 
